e491e0635de56a5ecfb1e466e4c4bf53abfce318
Eandalf <clinah@connect.ust.hk>
2020-11-03 23:22:57 +0800

    patch the layout bug

--- src/js/game.js
+++ src/js/game.js
@@ -1,8 +1,17 @@
 'use strict';
 $(document).ready(function () {
     // making the board a square
+    console.log($("#board").width());
+    console.log($("#board").height());
     var boardWidth = $("#board").width();
-    $("#main_grid").height(boardWidth);
+    var boardHeight = $("#board").height();
+    if (boardWidth <= boardHeight) {
+        $("#main_grid").height(boardWidth);
+    } else {
+        document.documentElement.style.setProperty("--board_width", "" + boardHeight + "px");
+        document.documentElement.style.setProperty("--grid_padding", "auto");
+    }
+
 
     // making the control panel a square
     var controlPanelWidth = $("#control_panel").width();
